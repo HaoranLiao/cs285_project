34f5584cec2f84995ff1fb9f9583a3b18036da36
haoranliao <haoran.liao@berkeley.edu>
2020-10-24 15:35:40 -0700

    cnn update

--- baselines/rnd_gail/rnd_critic.py
+++ baselines/rnd_gail/rnd_critic.py
@@ -49,10 +49,16 @@ class RND_Critic(object):
     #     return layer
 
     def build_graph(self, ob, ac, scope, hid_layer, hid_size, size):
+        fan_in = [4, 32, 64]
+        fan_out = [32, 64, 64]
+        low, high = [], []
+        for i in range(len(fan_in))
+            low.append(-np.sqrt(6.0/(fan_in[i] + fan_out[i])))
+            high.append(np.sqrt(6.0/(fan_in[i] + fan_out[i])))
         filters = [
-                    tf.Variable(tf.random_uniform((8, 8, 4, 32)), minval=low, maxval=high, dtype=tf.float32),
-                    tf.Variable(tf.random_uniform((4, 4, 32, 64)), minval=low, maxval=high, dtype=tf.float32),
-                    tf.Variable(tf.random_uniform((3, 3, 64, 64)), minval=low, maxval=high, dtype=tf.float32)
+                    tf.Variable(tf.random_uniform((8, 8, fan_in[0], fan_out[0])), minval=low[0], maxval=high[0], dtype=tf.float32),
+                    tf.Variable(tf.random_uniform((4, 4, fan_in[1], fan_out[1])), minval=low[1], maxval=high[1], dtype=tf.float32),
+                    tf.Variable(tf.random_uniform((3, 3, fan_in[2], fan_out[2])), minval=low[2], maxval=high[2], dtype=tf.float32)
                 ]
         strides = [4, 2, 1]
         with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
