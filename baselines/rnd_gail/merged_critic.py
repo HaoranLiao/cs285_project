d366a9d826ceb80b1f81299f3795489e3e20c3c7
Jerry <jerry-li1996@berkeley.edu>
2020-10-24 15:04:55 -0700

    update environment

--- baselines/rnd_gail/merged_critic.py
+++ baselines/rnd_gail/merged_critic.py
@@ -1,10 +1,13 @@
 import numpy as np
+from tensorflow.python.ops.gen_math_ops import exp
 from .rnd_critic import RND_Critic, Enc_Critic
 from .mmd_critic import MMD_Critic
 
 def make_critic(env, exp_data, hid_size=128, rnd_hid_size=128, reward_type=0, scale=250000):
-    ac_size = env.action_space.sample().shape[0]
-    ob_size = env.observation_space.shape[0]
+    # ac_size = env.action_space.sample().shape[0]
+    # ob_size = env.observation_space.shape[0]
+    ac_size = env.action_space.n
+    ob_size = exp_data[0].shape[1:]
     if reward_type == 0:
         critic = RND_Critic(ob_size, ac_size, hid_size=hid_size, rnd_hid_size=rnd_hid_size, scale=scale)
     elif reward_type == 1:
