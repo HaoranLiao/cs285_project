32515bba285a28cfce671429d2924749cc9c76d2
Haoran Liao <haoran@physics.mcgill.ca>
2020-12-06 16:44:39 -0800

    corrected

--- baselines/common/tf_util.py
+++ baselines/common/tf_util.py
@@ -26,8 +26,8 @@ def cnn():
 
     '''type 1'''
     # (64, 8, 2), (128, 6, 3), (128, 4, 2), (128, 3, 1)
-    # fan_in = [4, 64, 128]
-    # fan_out = [64, 128, 128]
+    # fan_in = [4, 64, 128, 128]
+    # fan_out = [64, 128, 128, 128]
     # low, high = [], []
     # for i in range(len(fan_in)):
     #     low.append(-np.sqrt(6.0 / (fan_in[i] + fan_out[i])))
@@ -36,7 +36,7 @@ def cnn():
     #     tf.Variable(tf.random_uniform((8, 8, fan_in[0], fan_out[0]), minval=low[0], maxval=high[0], dtype=tf.float32)),
     #     tf.Variable(tf.random_uniform((6, 6, fan_in[1], fan_out[1]), minval=low[1], maxval=high[1], dtype=tf.float32)),
     #     tf.Variable(tf.random_uniform((4, 4, fan_in[2], fan_out[2]), minval=low[2], maxval=high[2], dtype=tf.float32)),
-    #     tf.Variable(tf.random_uniform((3, 3, fan_in[2], fan_out[2]), minval=low[2], maxval=high[2], dtype=tf.float32))
+    #     tf.Variable(tf.random_uniform((3, 3, fan_in[3], fan_out[3]), minval=low[3], maxval=high[3], dtype=tf.float32))
     # ]
     # strides = [[1, 2, 2, 1], [1, 3, 3, 1], [1, 2, 2, 1], [1, 1, 1, 1]]  # batch, x, y, channel
     # cnn_type = 1
@@ -52,17 +52,11 @@ def cnn():
         tf.Variable(tf.random_uniform((8, 8, fan_in[0], fan_out[0]), minval=low[0], maxval=high[0], dtype=tf.float32)),
         tf.Variable(tf.random_uniform((4, 4, fan_in[1], fan_out[1]), minval=low[1], maxval=high[1], dtype=tf.float32)),
         tf.Variable(tf.random_uniform((4, 4, fan_in[2], fan_out[2]), minval=low[2], maxval=high[2], dtype=tf.float32)),
-        tf.Variable(tf.random_uniform((3, 3, fan_in[2], fan_out[2]), minval=low[2], maxval=high[2], dtype=tf.float32)),
-        tf.Variable(tf.random_uniform((2, 2, fan_in[2], fan_out[2]), minval=low[2], maxval=high[2], dtype=tf.float32))
+        tf.Variable(tf.random_uniform((3, 3, fan_in[3], fan_out[3]), minval=low[3], maxval=high[3], dtype=tf.float32)),
+        tf.Variable(tf.random_uniform((2, 2, fan_in[4], fan_out[4]), minval=low[4], maxval=high[4], dtype=tf.float32))
     ]
-    strides = [[1, 2, 2, 1], [1, 2, 2, 1], [1, 2, 2, 1], [1, 1, 1, 1], [1, 1, 1, 1]]  # batch, x, y, channel
+    strides = [[1, 2, 2, 1], [1, 2, 2, 1], [1, 2, 2, 1], [1, 1, 1, 1], [1, 1, 1, 1]]  # [batch, x, y, channel]
     cnn_type = 2
-
-
-
-
-
-
 
 
 
